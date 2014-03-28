72e0ece102d5b751d280417a678d5552d81cb57a
jgsmith <jgsmith@gmail.com>
2014-03-28 10:50:21 -0400

    change string to text column

--- db/migrate/20131231170747_create_item.rb
+++ db/migrate/20131231170747_create_item.rb
@@ -3,7 +3,7 @@ class CreateItem < ActiveRecord::Migration
     create_table :items do |t|
       t.string :archetype_name, null: false
       [ :traits, :skills, :stats, :details, :physicals, :counters, :resources, :flags ].each do |col|
-        t.string col, default: {}.to_yaml
+        t.text col, default: {}.to_yaml
       end
     end
   end
